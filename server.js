50e1be6ff0a32f7c3e593e3b19ddadfb079be8e0
Bradyk927 <Bradyk927@gmail.com>
2018-09-10 11:01:02 -0700

    changes

--- server.js
+++ server.js
@@ -3,6 +3,13 @@ const path = require("path");
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mongoose = require('mongoose');
+
+var mongoDB = 'usersdb';
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // Define middleware here
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -19,6 +26,11 @@ if (process.env.NODE_ENV === "production") {
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/db/usersdb";
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> Server now on port ${PORT}!`);
