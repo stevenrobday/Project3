ad1256b2d30bcad9ed4a5d67bd17b87106dfdcd4
stevenrobday <stevenrobday@gmail.com>
2018-09-12 10:34:20 -0700

    asdfa

--- server.js
+++ server.js
@@ -29,10 +29,8 @@ app.get("*", (req, res) => {
 });
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/gameguys");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/gamesplice");
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> Server now on port ${PORT}!`);
 });
-
-
