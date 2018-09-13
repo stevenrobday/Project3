c66cc521e0bd7c0bd2570ec7ce0394a730179f22
stevenrobday <stevenrobday@gmail.com>
2018-09-13 02:39:27 -0700

    dsaflokjasdflkjds

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const routes = require("./routes");
 
 app.all('*', function(req, res, next) {
   var origin = req.get('origin'); 
-  res.header('Access-Control-Allow-Origin', origin);
+  res.header('Access-Control-Allow-Origin', 'https://www.giantbomb.com/');
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
